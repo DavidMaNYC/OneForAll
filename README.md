31387692d0c64e640187d7eea631563e3ca782b6
unknown <David>
2021-11-23 22:29:25 -0500

    starter code

---
+++ README.md
@@ -0,0 +1,16 @@
+<img src=https://raw.githubusercontent.com/DavidMaNYC/One-For-All/main/resources/new-logo.png />
+
+# One-For-All
+Your one stop extension for VSCode to make searching the web easier.
+
+# What it does:
+Well, if you are stuck in some confusion about what this code does or what this function does, then you can simply highlight that line of code and you'll get instant search results for that query on your default web-browser without having to leave your IDE.
+
+## How it works!
+Simply select the word or function you want to search, then right click and hover over `Search` in drop-down menu. This will then give you options for several search engine browsers to look up from.
+
+<img src=https://raw.githubusercontent.com/DavidMaNYC/One-For-All/main/resources/Example.PNG />
+
+## How it opens in browser
+
+<img src=https://raw.githubusercontent.com/DavidMaNYC/One-For-All/main/resources/Example%202.png />
---
+++ src/extension.ts
@@ -0,0 +1,95 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.duckDuckGo",()=>{
+		  let link= "https://duckduckgo.com/?q=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+
+	  context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.google",
+		()=>{
+		  let link= "https://www.google.com/search?q=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+	
+	  context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.bing",
+		()=>{
+		  let link= "https://www4.bing.com/search?q=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+	
+	  context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.yahoo",
+		()=>{
+		  let link= "https://search.yahoo.com/search?p=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+	
+	  context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.baidu",
+		()=>{
+		  let link= "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+	
+	  context.subscriptions.push(vscode.commands.registerTextEditorCommand(
+		"extension.yandex",
+		()=>{
+		  let link= "https://yandex.com/search/?text=%SELECTION%";
+		  webSearch(link);
+		}
+	  ));
+	}
+
+	function webSearch(url:string) {
+		const selectedText = getSelectedText();
+		if (!selectedText) {
+		  return;
+		}
+		const uriText = encodeURI(selectedText);
+		const queryTemplate = url;
+		const query = queryTemplate.replace("%SELECTION%", uriText);
+		vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(query));
+	  }
+	  
+	  function getSelectedText() {
+		const documentText = vscode.window.activeTextEditor?.document.getText();
+		let language= vscode.window.activeTextEditor?.document.languageId;
+	  
+		if (!documentText) {
+		  return "";
+		}
+		if (language==="javascriptreact"){
+		  language= "javascript react";
+		}
+		if (language==="typescriptreact"){
+		  language= "typescript react";
+		}
+		const activeSelection = vscode.window.activeTextEditor?.selection;
+		if (activeSelection?.isEmpty) {
+		  return "";
+		}
+		const selStartoffset = vscode.window.activeTextEditor?.document.offsetAt(
+		  activeSelection.start
+		);
+		const selEndOffset = vscode.window.activeTextEditor?.document.offsetAt(
+		  activeSelection.end
+		);
+		let selectedText = documentText.slice(selStartoffset, selEndOffset).trim();
+	  
+		return `${selectedText.replace(/\s\s+/g, " ")} ${language}`;
+	  }
+// this method is called when your extension is deactivated
+export function deactivate() {}
